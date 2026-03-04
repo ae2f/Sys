dd433dafec36dad9b6d40f522b90d596879a72de
dalmurii <mincraft20046@gmail.com>
2026-03-04 21:33:00 +0900

    UPDATE: Shrinking test attempt
     Changes to be committed:
    	- modified:   test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@
 
 makers=("-GNinja" "")
 lstdc=("90" "99" "11" "17" "23")
-lstdcc=("98" "11" "14" "17" "20" "23")
 
 __ae2f_CXX=("ON")
 __ae2f_IS_SHARED=("ON" "OFF")
@@ -13,7 +12,6 @@ __ae2f_IS_SHARED=("ON" "OFF")
 buildtype=$3
 
 for stdc in ${lstdc[@]}; do
-for stdcc in ${lstdcc[@]}; do
 for maker in ${makers[@]}; do
 for _ae2f_CXX in ${__ae2f_CXX[@]}; do
 for _ae2f_IS_SHARED in ${__ae2f_IS_SHARED[@]}; do
@@ -40,7 +38,6 @@ for _ae2f_MAC_BUILD in ${__ae2f_IS_SHARED[@]}; do
 		echo "Test failed"
 			exit 1
 		}
-done
 done
 done
 done
